43792034f14c5cb800efd4375c7a21be39bf61d9
fan225 <fan225@gmail.com>
2018-07-22 15:30:13 -0600

    leet278 AC

--- leetcode/leet34.cpp
+++ leetcode/leet34.cpp
@@ -1,30 +1,44 @@
 class Solution {
-    int lower_bound(const vector<int>& a, const int x) {
+    int lower_bound(const vector<int>& A, const int t) {
         int lo=0;
-        int hi=a.size()-1;
+        int hi=A.size()-1;
         while (lo<hi){
             int mid=lo+(hi-lo)/2;
-            if (x<=a[mid]) {
+            if (t<=A[mid]) {
                 hi=mid;
             } else {
                 lo=mid+1;
             }
         }
-        return x<=a[lo]?lo:a.size();
+        return t<=A[lo]?lo:A.size();
     }
 
-    int upper_bound(const vector<int>& a, const int x) {
+    int upper_bound(const vector<int>& A, const int t) {
         int lo=0;
-        int hi=a.size()-1;
+        int hi=A.size()-1;
         while (lo<hi){
             int mid=lo+(hi-lo)/2;
-            if (x<a[mid]) {
+            if (t<A[mid]) {
                 hi=mid;
             } else {
                 lo=mid+1;
             }
         }
-        return x<a[lo]?lo:a.size();
+        return t<A[lo]?lo:A.size();
+    }
+
+    int last_le(const vector<int>& A, const int t) {
+        int lo=0;
+        int hi=A.size()-1;
+        while (lo<hi){
+            int mid=lo+(hi-lo+1)/2;
+            if (A[mid]<=t) {
+                lo=mid;
+            } else {
+                hi=mid-1;
+            }
+        }
+        return A[lo]<=t?lo:-1;
     }
 
     public:
@@ -32,7 +46,8 @@ class Solution {
         if (nums.empty()) return {-1,-1};
         int first=lower_bound(nums, target);
         if (first>=nums.size() || nums[first]!=target) return {-1,-1};
-        int last=upper_bound(nums, target)-1;
+        //int last=upper_bound(nums, target)-1;
+        int last=last_le(nums, target);
         return {first, last};
     }
 };
